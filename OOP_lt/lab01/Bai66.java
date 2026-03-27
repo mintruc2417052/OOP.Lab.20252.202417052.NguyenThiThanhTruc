810d1fdeea0b606d48e9fbda23e15c7130f57d85
ThanhTruc <truc.ntt2417052@sis.hust.edu.vn>
2026-03-27 08:09:35 +0700

    up

--- OOP_lt/lab01/Bai66.java
+++ OOP_lt/lab01/Bai66.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Bai64 {
+public class Bai66 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
